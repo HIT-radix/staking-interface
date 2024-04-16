bd98bc4610fc1fc19f6750061b92a22d850e021d
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2024-04-17 03:02:40 +0500

    feat: added metadata of website

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -2,7 +2,7 @@ import hitLogo from "Assets/Images/hit-logo.png";
 const Home = () => {
   return (
     <div className="preloader-container bouncing">
-      <img src={hitLogo} alt="meshIcon" className="w-[200px] h-[200px] mt-2" />
+      <img src={hitLogo} alt="meshIcon" className="w-[200px] h-[200px]" />
     </div>
   );
 };
