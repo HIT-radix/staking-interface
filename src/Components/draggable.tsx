325ad29d41fb61391e92454757997f1176c0b4b2
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2024-07-31 20:29:03 +0500

    progress: logic for dragging and clicking complete

---
+++ src/Components/draggable.tsx
@@ -0,0 +1,110 @@
+import { useEffect, useState } from "react";
+import Draggable from "react-draggable";
+
+import lock from "Assets/Svgs/lock-white.svg";
+import LockedHitsModal from "./Modals/lockedHits";
+
+const SnappableDraggable = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [dragging, setDragging] = useState(false);
+  const [bounds, setBounds] = useState({
+    left: 0,
+    top: 0,
+    right: window.innerWidth,
+    bottom: window.innerHeight,
+  });
+
+  const handleStop = (e: any, data: any) => {
+    eventControl(e);
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+    const elementWidth = data.node.offsetWidth;
+    const elementHeight = data.node.offsetHeight;
+    console.log("elementHeight", elementHeight);
+    setBounds({ ...bounds, bottom: screenHeight - elementHeight });
+
+    const newX = data.x < screenWidth / 2 ? 0 : screenWidth - elementWidth;
+    // const newY = data.y < screenHeight / 2 ? 0 : screenHeight - elementHeight;
+    const newY = data.y;
+
+    setPosition({ x: newX, y: newY });
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setBounds({
+        left: 0,
+        top: 0,
+        right: window.innerWidth,
+        bottom: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleClick = () => {
+    if (!dragging) {
+      (document.getElementById("my_modal_2") as HTMLDialogElement).showModal(); // Perform the desired action here
+    }
+  };
+
+  const eventControl = (event: any) => {
+    if (event.type === "mousemove" || event.type === "touchmove") {
+      setDragging(true);
+    }
+
+    if (event.type === "mouseup" || event.type === "touchend") {
+      setTimeout(() => {
+        setDragging(false);
+      }, 100);
+    }
+  };
+
+  return (
+    <>
+      <div
+        className="fixed z-40 "
+        onClick={handleClick}
+        // onMouseUp={() => (document.getElementById("my_modal_2") as HTMLDialogElement).showModal()}
+        onTouchEnd={handleClick}
+      >
+        <Draggable position={position} onDrag={eventControl} onStop={handleStop} bounds={bounds}>
+          <div
+            className="w-[25vw] h-[25vw] max-w-[150px] max-h-[150px] bg-contain bg-center flex items-center justify-center cursor-pointer"
+            style={{
+              transition: "transform 0.1s linear",
+              backgroundImage: `url(${lock})`,
+              transform: `translate(${position.x}px, ${position.y}px)`,
+              backgroundRepeat: "no-repeat",
+            }}
+          >
+            <div
+              className="text-center mt-[25%]"
+              style={{
+                fontSize:
+                  window.innerWidth - 0.98 * window.innerWidth < 12
+                    ? 12
+                    : window.innerWidth - 0.98 * window.innerWidth > 20
+                    ? 20
+                    : window.innerWidth - 0.98 * window.innerWidth,
+                //   fontSize: 20,
+              }}
+            >
+              <p className="font-bold">45% HIT</p>
+              <p className="font-bold">Locked!</p>
+            </div>
+            {/* <img src={lock} alt="lock" /> */}
+          </div>
+        </Draggable>
+      </div>
+      <LockedHitsModal />
+    </>
+  );
+};
+
+export default SnappableDraggable;
--- src/Layout/mainlayout.tsx
+++ src/Layout/mainlayout.tsx
@@ -2,10 +2,12 @@ import { ReactNode } from "react";
 
 import Header from "./header";
 import Footer from "./footer";
+import SnappableDraggable from "Components/draggable";
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
     <>
+      <SnappableDraggable />
       {<Header />}
       {children}
       <Footer />
