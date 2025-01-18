2a4b09b002f29e2548694bbab7b166db807f1cca
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-01-19 03:58:26 +0500

    feat: update SnapshotModal to display confirmation message and remove save button

--- src/Pages/Topup/Components/snapshotModal.tsx
+++ src/Pages/Topup/Components/snapshotModal.tsx
@@ -1,28 +1,18 @@
 import DialogStructure from "Components/dialogStructure";
 import DistributionTable from "./distributionTable";
 import { useSelector } from "Store";
-import { Save } from "lucide-react";
 
 const SnapshotModal = () => {
   const rewardsModalData = useSelector((state) => state.session.rewardsModalData);
 
-  const onSaveClick = async () => {
-    if (!rewardsModalData) return;
-    (document.getElementById("SnapshotModal") as HTMLDialogElement).close();
-  };
   return (
     <DialogStructure modalId="SnapshotModal">
       {rewardsModalData && (
         <>
-          <p className="text-secondary text-xl font-semibold text-center mb-3">Save Snapshot</p>
+          <p className="text-secondary text-xl font-semibold text-center mb-3">Snapshot Saved!</p>
           <DistributionTable distribution={rewardsModalData.RewardTokenDistributions} />
         </>
       )}
-      <div className="flex justify-center mt-5">
-        <button className="btn bg-accent text-base-100 hover:bg-accent/50" onClick={onSaveClick}>
-          Save! <Save />
-        </button>
-      </div>
     </DialogStructure>
   );
 };
