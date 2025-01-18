b2b67981df58d24613270c2fddaf3769347dbf62
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-01-18 23:57:33 +0500

    feat: implement snapshot functionality and xUSDT management in InvestmentFunds component

--- src/Pages/Topup/Components/investmentFunds.tsx
+++ src/Pages/Topup/Components/investmentFunds.tsx
@@ -1,5 +1,69 @@
+import GeneralOwnerInterface from "Components/generalOwnerInterface";
+import { Camera } from "lucide-react";
+import { StakingTokens } from "Types/reducers";
+import { getRdt } from "subs";
+import axios from "axios";
+import { SnapshotApiResponse } from "Types/api";
+import { HIT_SERVER_URL } from "Constants/endpoints";
+import { setRewardsModalData } from "Store/Reducers/session";
+import { XUSDT_RESOURCE_ADDRESS } from "Constants/address";
+import { dispatch } from "Store";
+import { DataRequestBuilder } from "@radixdlt/radix-dapp-toolkit";
+
 const InvestmentFunds = () => {
-  return <div>InvestmentFunds</div>;
+  const takeSnapshot = async () => {
+    const rdtInstance = getRdt();
+    if (rdtInstance) {
+      rdtInstance.walletApi.dataRequestControl(async ({ proofs }) => {
+        const {
+          data: { rewardsList },
+        } = await axios.post<SnapshotApiResponse>(`${HIT_SERVER_URL}/node-staking/take-snapshot`, {
+          proofs,
+          reward: 1,
+        });
+
+        dispatch(
+          setRewardsModalData({
+            amount: "1",
+            RewardTokenDistributions: rewardsList,
+            tokenSymbol: StakingTokens.XUSDT,
+            tokenAddress: XUSDT_RESOURCE_ADDRESS,
+          })
+        );
+        (document.getElementById("SnapshotModal") as HTMLDialogElement).showModal();
+
+        // reset this so it does not trigger on wallet connect button
+        rdtInstance.walletApi.dataRequestControl(async () => {});
+      });
+
+      await rdtInstance.walletApi.sendOneTimeRequest(
+        DataRequestBuilder.accounts().atLeast(1).withProof()
+      );
+    }
+  };
+  return (
+    <div className="flex flex-col items-center justify-center text-accent border">
+      <div className="btn btn-accent mb-6 mt-3" onClick={takeSnapshot}>
+        Take Snapshot <Camera />
+      </div>
+      <GeneralOwnerInterface
+        heading="Lock xUSDT for Future Rewards"
+        placeholder="Enter xUSDT amount to lock"
+        balance={"0"}
+        onButtonClick={async (amount) => {}}
+        btnText="Lock xUSDT tokens"
+        tokenSymbol={StakingTokens.XUSDT}
+      />
+      <GeneralOwnerInterface
+        heading="Distribute locked xUSDT"
+        placeholder="Enter xUSDT amount to distribute"
+        balance={"0"}
+        onButtonClick={async (amount) => {}}
+        btnText="Distribute xUSDT"
+        tokenSymbol={StakingTokens.XUSDT}
+      />
+    </div>
+  );
 };
 
 export default InvestmentFunds;
