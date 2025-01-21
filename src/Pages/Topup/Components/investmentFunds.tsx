a791a477e826ced3dbf8923f7096e78412e11a83
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-01-21 17:54:49 +0500

    feat: integrate selectedSnapshots state into InvestmentFunds for xUSDT distribution

--- src/Pages/Topup/Components/investmentFunds.tsx
+++ src/Pages/Topup/Components/investmentFunds.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { ArrowRight, Camera, Save } from "lucide-react";
 import axios from "axios";
 import { DataRequestBuilder } from "@radixdlt/radix-dapp-toolkit";
@@ -9,12 +10,13 @@ import { SnapshotApiResponse } from "Types/api";
 import { HIT_SERVER_URL } from "Constants/endpoints";
 import { incrementSuccessTxCount, setRewardsModalData } from "Store/Reducers/session";
 import { XUSDT_RESOURCE_ADDRESS } from "Constants/address";
-import { dispatch } from "Store";
+import { dispatch, useSelector } from "Store";
 import SnapshotsTable from "./snapshotsTable";
 import { depositNodeStakingRewards } from "Utils/txSenders";
 import { calculateAvgShareOfSnapshots } from "Utils/format";
 
 const InvestmentFunds = () => {
+  const selectedRows = useSelector((state) => state.session.selectedSnapshots);
   const takeSnapshot = async () => {
     const rdtInstance = getRdt();
     if (rdtInstance) {
@@ -52,6 +54,8 @@ const InvestmentFunds = () => {
     }
   };
 
+  const enableDistribution = useMemo(() => selectedRows.length > 0, [selectedRows]);
+
   return (
     <div className="flex flex-col items-center justify-center text-accent">
       <div className="btn btn-accent mb-6 mt-3" onClick={takeSnapshot}>
@@ -67,14 +71,24 @@ const InvestmentFunds = () => {
         btnText="Lock xUSDT tokens"
         tokenSymbol={StakingTokens.XUSDT}
       />
-      <GeneralOwnerInterface
-        heading="Distribute locked xUSDT"
-        placeholder="Enter xUSDT amount to distribute"
-        balance={"9999999"}
-        onButtonClick={async (amount) => await calculateAvgShareOfSnapshots(amount)}
-        btnText="Distribute xUSDT"
-        tokenSymbol={StakingTokens.XUSDT}
-      />
+      <div
+        onClick={() => {
+          if (!enableDistribution) {
+            alert("Please select a snapshot to distribute xUSDT");
+          }
+        }}
+      >
+        <div className={enableDistribution ? "" : "opacity-40 pointer-events-none "}>
+          <GeneralOwnerInterface
+            heading="Distribute locked xUSDT"
+            placeholder="Enter xUSDT amount to distribute"
+            balance={"9999999"}
+            onButtonClick={async (amount) => await calculateAvgShareOfSnapshots(amount)}
+            btnText="Distribute xUSDT"
+            tokenSymbol={StakingTokens.XUSDT}
+          />
+        </div>
+      </div>
       <div className="w-[95vw] max-w-[700px]">
         <SnapshotsTable />
       </div>
