378985d0eda2b41d7bb260abd1904ec3a8dad292
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-03-11 16:52:51 +0500

    fix: update HIT_SERVER_URL to point to the correct server endpoint

--- src/Constants/endpoints.ts
+++ src/Constants/endpoints.ts
@@ -10,6 +10,6 @@ export const networkRPC =
 // export const HIT_SERVER_URL =
 //   process.env.REACT_APP_ENVIRONMENT === Environment.dev
 //     ? "http://localhost:3002"
-//     : "https://bot.addix.meme";
+//     : "https://server.addix.meme";
 
-export const HIT_SERVER_URL = "https://bot.addix.meme";
+export const HIT_SERVER_URL = "https://server.addix.meme";
