980d715cfddcde17027eec1fd424f78debb83324
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-04-13 15:31:07 +0500

    feat: enhance RootInvestment to fetch and include xUSDC liquidity values

--- src/Classes/investments/root.tsx
+++ src/Classes/investments/root.tsx
@@ -1,58 +1,85 @@
 import CachedService from "Classes/cachedService";
-import { ROOT_CDP_NFT_ADDRESS, ROOT_xUSDT_POOL_ADDRESS } from "Constants/address";
+import {
+  ROOT_CDP_NFT_ADDRESS,
+  ROOT_xUSDC_POOL_ADDRESS,
+  ROOT_xUSDT_POOL_ADDRESS,
+  XUSDC_RESOURCE_ADDRESS,
+  XUSDT_RESOURCE_ADDRESS,
+} from "Constants/address";
 import Decimal from "decimal.js";
 import { store } from "Store";
 import { InvestmentInfo } from "Types/misc";
 
 class RootInvestment {
   public async getInvestment(): Promise<InvestmentInfo> {
-    const { xusdt } = await this.fetchAllLiquidityValues();
-    const total = new Decimal(xusdt).toString();
+    const { xusdt, xusdc } = await this.fetchAllLiquidityValues();
+    const total = new Decimal(xusdt).add(xusdc).toString();
     return {
       platform: "Root Finance",
       total,
-      breakdown: [{ asset: "xUSDT", value: xusdt }],
+      breakdown: [
+        { asset: "xUSDT", value: xusdt },
+        { asset: "xUSDC", value: xusdc },
+      ],
       index: 2, // Update index as needed
     };
   }
 
   private async fetchAllLiquidityValues() {
-    const xusdt = await this.fetchxUSDTPoolLiquidityValue();
-    return { xusdt };
-  }
-
-  private async fetchxUSDTPoolLiquidityValue() {
     const felixNFTs = store.getState().session.felixWallet.nonFungible;
-    const nftId = felixNFTs[ROOT_CDP_NFT_ADDRESS].ids[0];
-    const assets = await this.fetchLiquidityReceiptData(nftId);
-    const unitToAssetRatio = await this.getUnitToAssetRatio();
-    if (!Number.isNaN(+assets) && !Number.isNaN(+unitToAssetRatio)) {
-      return new Decimal(assets).div(unitToAssetRatio).toString();
-    }
-    return "0";
-  }
+    const nftIds = felixNFTs[ROOT_CDP_NFT_ADDRESS].ids;
+    const assetsWithValues = await this.fetchLiquidityReceiptsData(nftIds);
 
-  private async fetchLiquidityReceiptData(nftid: string) {
-    const res = await CachedService.gatewayApi.state.getNonFungibleData(
-      ROOT_CDP_NFT_ADDRESS,
-      nftid
-    );
-    if (res.data?.programmatic_json.kind === "Tuple") {
-      const collateralData = res.data?.programmatic_json.fields[6];
-      if (
-        collateralData.kind === "Map" &&
-        collateralData.entries[0].value.kind === "PreciseDecimal"
-      ) {
-        return collateralData.entries[0].value.value;
-      }
-    }
-    return "0";
-  }
-
-  private async getUnitToAssetRatio() {
-    const res = await CachedService.gatewayApi.state.getEntityDetailsVaultAggregated(
+    const xusdt = await this.fetchPoolLiquidityValue(
+      assetsWithValues[XUSDT_RESOURCE_ADDRESS],
       ROOT_xUSDT_POOL_ADDRESS
     );
+    const xusdc = await this.fetchPoolLiquidityValue(
+      assetsWithValues[XUSDC_RESOURCE_ADDRESS],
+      ROOT_xUSDC_POOL_ADDRESS
+    );
+
+    return { xusdt, xusdc };
+  }
+
+  private async fetchPoolLiquidityValue(assetValue: string = "0", poolAddress: string) {
+    const unitToAssetRatio = await this.getUnitToAssetRatio(poolAddress);
+    if (!Number.isNaN(+assetValue) && !Number.isNaN(+unitToAssetRatio)) {
+      return new Decimal(assetValue).div(unitToAssetRatio).toString();
+    }
+    return "0";
+  }
+
+  private async fetchLiquidityReceiptsData(nftids: string[]) {
+    const res = await CachedService.gatewayApi.state.getNonFungibleData(
+      ROOT_CDP_NFT_ADDRESS,
+      nftids
+    );
+    let assetsData: Record<string, string> = {};
+
+    res.forEach((itemRes) => {
+      let tokenAddress = "";
+      let assetValue = "0";
+      if (itemRes.data?.programmatic_json.kind === "Tuple") {
+        const collateralData = itemRes.data?.programmatic_json.fields[6];
+        if (
+          collateralData.kind === "Map" &&
+          collateralData.entries[0].value.kind === "PreciseDecimal"
+        ) {
+          assetValue = collateralData.entries[0].value.value;
+        }
+        if (collateralData.kind === "Map" && collateralData.entries[0].key.kind === "Reference") {
+          tokenAddress = collateralData.entries[0].key.value;
+        }
+      }
+      assetsData[tokenAddress] = assetValue;
+    });
+
+    return assetsData;
+  }
+
+  private async getUnitToAssetRatio(poolAddress: string) {
+    const res = await CachedService.gatewayApi.state.getEntityDetailsVaultAggregated(poolAddress);
 
     if (res.details?.type === "Component" && res.details.state && "fields" in res.details.state) {
       const fields = res.details.state.fields as any[];
