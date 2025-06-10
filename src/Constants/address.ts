1c83d6279fd740d26403885ed479e93153c305c2
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-06-10 15:46:57 +0500

    feat: add NODE_STAKING_REDDICKS_KEY_VALUE_STORE_ADDRESS constant

--- src/Constants/address.ts
+++ src/Constants/address.ts
@@ -78,6 +78,9 @@ export const NODE_STAKING_XUSDT_KEY_VALUE_STORE_ADDRESS =
 export const NODE_STAKING_XUSDC_KEY_VALUE_STORE_ADDRESS =
   "internal_keyvaluestore_rdx1kpgllfccyumfjs4l6k97seux56csehvt8u7geqnqtm3f7g6kuzn7aj";
 
+export const NODE_STAKING_REDDICKS_KEY_VALUE_STORE_ADDRESS =
+  "internal_keyvaluestore_rdx1kzf0t4fwp6wzxndtw0q6qkwrt5hrsquemtpd0nvp8gajms89caf3vm";
+
 export const NODE_STAKING_FOMO_KEY_VALUE_STORE_ADDRESS =
   process.env.REACT_APP_ENVIRONMENT === Environment.dev
     ? "internal_keyvaluestore_tdx_2_1kzlxcs94slutg6raf9gc9kde7xyv7zd2dkqfkx0p2n80uhl88esah5"
