8bc1924c04e4942bbb1059c34f735340bcdbda33
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-07-06 16:03:48 +0500

    feat: add fetchAttosPools and extractAPYsFromAttosPools methods to APYCache

--- src/Classes/apyCache.ts
+++ src/Classes/apyCache.ts
@@ -1,11 +1,12 @@
 import localforage from "localforage";
 
 import { store } from "../Store";
-import { AttosStrategyData, StrategyId, SurgeStatsResponse } from "Types/api";
+import { AttosPoolData, AttosStrategyData, StrategyId, SurgeStatsResponse } from "Types/api";
 import axios from "axios";
 import { setApyFetching } from "Store/Reducers/loadings";
 import { ATTO_BASE_URL, SURGE_BASE_URL } from "Constants/endpoints";
 import { setLastAPYsUpdated } from "Store/Reducers/app";
+import { calculateYearlyAPY } from "Utils/format";
 
 class APYCache {
   allAPYs: Record<string, number> = {};
@@ -40,12 +41,14 @@ class APYCache {
 
   fetchAllAPYs = async () => {
     store.dispatch(setApyFetching(true));
-    const [attosStrategies, surgeApy] = await Promise.all([
+    const [attosStrategies, surgeApy, attosPools] = await Promise.all([
       this.fetchAttosStrategies(),
       this.fetchSurgeApy(),
+      this.fetchAttosPools(),
     ]);
-    const attosApys = this.extractAPYsFromStrategies(attosStrategies);
-    const allAPYs = { ...attosApys, ...surgeApy };
+    const attosStrategiesApys = this.extractAPYsFromStrategies(attosStrategies);
+    const attosPoolsApys = this.extractAPYsFromAttosPools(attosPools);
+    const allAPYs = { ...attosStrategiesApys, ...surgeApy, ...attosPoolsApys };
     await this.setAPYs(allAPYs);
     this.allAPYs = allAPYs;
     store.dispatch(setApyFetching(false));
@@ -65,7 +68,10 @@ class APYCache {
 
   async fetchAttosStrategies(): Promise<AttosStrategyData[]> {
     try {
-      const response = await axios.get<AttosStrategyData[]>(ATTO_BASE_URL + "/strategies");
+      const response = await axios.get<AttosStrategyData[]>(ATTO_BASE_URL + "/v2/strategies");
+      if (response.status !== 200) {
+        return [];
+      }
       return response.data;
     } catch (error) {
       console.error("Failed to fetch Attos strategies:", error);
@@ -87,6 +93,31 @@ class APYCache {
       return {};
     }
   }
+
+  async fetchAttosPools(): Promise<AttosPoolData[]> {
+    try {
+      const response = await axios.get<AttosPoolData[]>(ATTO_BASE_URL + "/pools");
+      if (response.status !== 200) {
+        return [];
+      }
+      return response.data;
+    } catch (error) {
+      console.error("Failed to fetch Attos strategies:", error);
+      return [];
+    }
+  }
+
+  extractAPYsFromAttosPools = (pools: AttosPoolData[]) => {
+    const apyObj: Record<string, number> = {};
+    const validKeys = new Set(Object.values(StrategyId));
+    pools.forEach((pool) => {
+      const key = `${pool.left_alt}_${pool.right_alt}_${pool.type}_pool` as StrategyId;
+      if (validKeys.has(key)) {
+        apyObj[key] = calculateYearlyAPY(pool.bonus_7d);
+      }
+    });
+    return apyObj;
+  };
 }
 
 const ApyCacheService = new APYCache();
