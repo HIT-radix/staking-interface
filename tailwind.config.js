f12737429cb209eb659a8bf8efdac6d70cf7b74b
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-08-15 12:42:36 +0500

    refactor: add Paytone One font family to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        paytone: ['"Paytone One"', "sans-serif"],
+      },
+    },
   },
   daisyui: {
     themes: [
