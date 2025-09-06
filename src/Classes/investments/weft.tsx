fdcf2db61b2c4ece23221c826b019c9a2189dea9
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-09-06 20:57:31 +0500

    feat: enhance WeftInvestment to include hUSDC handling and calculations

--- src/Classes/investments/weft.tsx
+++ src/Classes/investments/weft.tsx
@@ -1,5 +1,7 @@
 import {
+  hUSDC_RESOURCE_ADDRESS,
   WEFT_KEY_VALUE_STORE_ADDRESS,
+  WEFT_W2_hUSDC_RESOURCE_ADDRESS,
   WEFT_W2_xUSDC_RESOURCE_ADDRESS,
   WEFT_W2_xUSDT_RESOURCE_ADDRESS,
   XUSDC_RESOURCE_ADDRESS,
@@ -11,6 +13,7 @@ import Decimal from "decimal.js";
 import { InvestmentInfo } from "Types/misc";
 import xusdtLogo from "Assets/Images/xUSDT.png";
 import xusdcLogo from "Assets/Images/xUSDC.png";
+import husdcLogo from "Assets/Svgs/husdc.svg";
 import { StrategyId } from "Types/api";
 
 class WeftInvestment {
@@ -18,8 +21,13 @@ class WeftInvestment {
     const res = await this.fetchInvestment();
     const weftUSDTInvestment = res.usdt;
     const weftUSDCInvestment = res.usdc;
+    const weftHUSDCInvestment = res.husdc;
 
-    const totalInvestment = new Decimal(weftUSDTInvestment).add(weftUSDCInvestment).toString();
+    const totalInvestment = new Decimal(weftUSDTInvestment)
+      .add(weftUSDCInvestment)
+      .add(weftHUSDCInvestment)
+      .toString();
+
     return {
       platform: "Weft Finance",
       total: totalInvestment,
@@ -40,6 +48,14 @@ class WeftInvestment {
           position: "xUSDC lend",
           apyId: StrategyId.xUSDC_Weft_Finance_Lending,
         },
+        {
+          asset: "hUSDC",
+          value: weftHUSDCInvestment,
+          logo: husdcLogo,
+          platform: "Weft Finance",
+          position: "hUSDC lend",
+          apyId: StrategyId.hUSDC_Weft_Finance_Lending,
+        },
       ],
       index: 1, // Update index as needed
     };
@@ -49,6 +65,7 @@ class WeftInvestment {
     const felixFungibles = store.getState().session.felixWallet.fungible;
     const weftXUSDTamount = felixFungibles[WEFT_W2_xUSDT_RESOURCE_ADDRESS]?.amount ?? "0";
     const weftXUSDCamount = felixFungibles[WEFT_W2_xUSDC_RESOURCE_ADDRESS]?.amount ?? "0";
+    const weftHUSDCamount = felixFungibles[WEFT_W2_hUSDC_RESOURCE_ADDRESS]?.amount ?? "0";
 
     const data = await CachedService.gatewayApi.state.innerClient.keyValueStoreData({
       stateKeyValueStoreDataRequest: {
@@ -56,11 +73,12 @@ class WeftInvestment {
         keys: [
           { key_json: { kind: "Reference", value: XUSDT_RESOURCE_ADDRESS } },
           { key_json: { kind: "Reference", value: XUSDC_RESOURCE_ADDRESS } },
+          { key_json: { kind: "Reference", value: hUSDC_RESOURCE_ADDRESS } },
         ],
       },
     });
 
-    const [unitRatioXUSDT, unitRatioXUSDC] = data.entries.map((entry) => {
+    const [unitRatioXUSDT, unitRatioXUSDC, unitRatioHUSDC] = data.entries.map((entry) => {
       if (entry.value.programmatic_json.kind === "Tuple") {
         const depositStateField = entry.value.programmatic_json.fields[5];
         if (
@@ -74,7 +92,8 @@ class WeftInvestment {
     });
     const weftUSDTInvestment = new Decimal(weftXUSDTamount).div(unitRatioXUSDT).toString();
     const weftUSDCInvestment = new Decimal(weftXUSDCamount).div(unitRatioXUSDC).toString();
-    return { usdt: weftUSDTInvestment, usdc: weftUSDCInvestment };
+    const weftHUSDCInvestment = new Decimal(weftHUSDCamount).div(unitRatioHUSDC).toString();
+    return { usdt: weftUSDTInvestment, usdc: weftUSDCInvestment, husdc: weftHUSDCInvestment };
   }
 }
 const weftInvestor = new WeftInvestment();
